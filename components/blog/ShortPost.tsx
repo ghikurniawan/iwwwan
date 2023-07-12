30aa7ec449b01fc60ac663d02208f05d90914e2f
ikurniawan <business.ikurniawan@gmail.com>
2023-07-12 13:41:09 +0700

    fix short blog

---
+++ components/blog/ShortPost.tsx
@@ -0,0 +1,28 @@
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+
+const ShortPost = () => {
+    return (
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Short blog" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="apple">Latest Posts</SelectItem>
+            <SelectItem value="banana">Newest Posts</SelectItem>
+            <SelectItem value="blueberry">Most popular posts</SelectItem>
+            <SelectItem value="grapes">By like count</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    )
+  }
+
+export default ShortPost
