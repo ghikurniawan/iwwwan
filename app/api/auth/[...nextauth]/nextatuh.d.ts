9bbb423c20a7c6f6996fd6073f8546b7dc1f4512
ikurniawan <business.ikurniawan@gmail.com>
2023-07-13 18:04:00 +0700

    fix eslint error

--- app/api/auth/[...nextauth]/nextatuh.d.ts
+++ app/api/auth/[...nextauth]/nextatuh.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import NextAuth from 'next-auth';
 
 declare module 'next-auth' {
@@ -15,5 +17,5 @@ declare module 'next-auth/adapters' {
       id: string;
       username: string;
       role: string;
-    };
+    }
 }
